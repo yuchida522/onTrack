5f592a8bf8959b80c9a911306c6e9fce93556834
yuchida522 <yuchida522@gmail.com>
2020-11-05 21:06:31 +0000

    added date feature to race search

--- seed_database.py
+++ seed_database.py
@@ -14,16 +14,3 @@ os.system('createdb races')
 model.connect_to_db(server.app)
 model.db.create_all()
 
-url = "http://api.amp.active.com/v2/search?sort=date_asc&api_key=dfquqhqma2ybzxe9p8usc4ys&start_date=2021-01-01..2021-09-26&query=half%20marathon&category=event&radius=500&near=San%20Francisco,CA,US&per_page=1000"
-
-payload = {}
-headers = {
-  'Cookie': 'TS01a59dd9=01572f3dbe4adb0e784043aa112651bd049edc0f0527e2eaab974b4e4024d57395ab0d1b5c1bfcedc4a29c97fd16813f89cb9df8b8'
-}
-
-response = requests.request("GET", url, headers=headers, data = payload)
-
-print(response.text.encode('utf8'))
-races_in_db = []
-
-
--- server.py
+++ server.py
@@ -31,11 +31,13 @@ def race_results():
 
     city_name = request.args.get('city_name', '')
     distance_length = request.args.get('distance_length', '')
+    start_date = request.args.get('start_date', '')
 
-    url = 'http://api.amp.active.com/v2/search?query=running&start_date=2021-02-01..2021-12-26&'
+    url = 'http://api.amp.active.com/v2/search?query=running'
     payload = {'api_key': API_KEY,
                'near': city_name,
-               'query': distance_length
+               'query': distance_length,
+               'start_date': start_date + '..'
                }
     
     response = requests.get(url, params=payload)
@@ -43,6 +45,7 @@ def race_results():
 
     events = data['results']
 
+    
     return render_template('race_results.html',
                             data=data,
                             results=events)
